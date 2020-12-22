c28c1169d65ab46e31c0858893c074105edcfed9
okudera <appledev.yuoku@gmail.com>
2020-12-22 09:55:04 +0900

    Initial Commit

---
+++ PopToRootDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopToRootDemo
+//
+//  Created by okudera on 2020/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
